4f600d8504c4d80038b580261e26ec080265d00c
B.Aden <belekovaden31@gmail.com>
2023-03-06 17:20:56 +1100

    ready in deploy site

--- smysl3/smysl3/settings.py
+++ smysl3/smysl3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fcz4prdno3#cn$!!^i3o_^)*+=(7a-mo_47h5sn#)m6o^7mkjf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'smysl3.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- smysl3/smysl3/urls.py
+++ smysl3/smysl3/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import os
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 os.chdir(r'C:\Users\Lenovo\PycharmProjects\youtubeDjango\smysl3')
 from django.contrib import admin
 from django.urls import path
@@ -24,3 +28,5 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('blog/<slug:slug>', views.article_page, name='article_page')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
