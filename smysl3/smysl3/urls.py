67855508b193b3df6dbf9d239a389b39a72dd3c1
B.Aden <belekovaden31@gmail.com>
2023-03-06 17:04:28 +1100

    add pip freeze to requirements.txt

--- smysl3/smysl3/urls.py
+++ smysl3/smysl3/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import os
-os.chdir(r'smysl3/')
+os.chdir(r'C:\Users\Lenovo\PycharmProjects\youtubeDjango\smysl3')
 from django.contrib import admin
 from django.urls import path
 from blog import views
